5a340becc6b8308497037f6ac648b715e705817f
a91490601-gif <a91490601@gmail.com>
2025-09-06 14:14:18 +0900

    Update supabaseClient.ts

--- web/src/lib/supabaseClient.ts
+++ web/src/lib/supabaseClient.ts
@@ -1,10 +1,3 @@
-/// web/src/lib/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);/ src/lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
