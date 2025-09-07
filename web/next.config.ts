4343f39c67b04f71990d4ed5e18bc2d4a20102ec
a91490601-gif <a91490601@gmail.com>
2025-09-07 16:32:42 +0900

    Update next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,9 +1,3 @@
-// next.config.ts
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  eslint: { ignoreDuringBuilds: true },      // ESLint 때문에 빌드 멈추지 않게
-  typescript: { ignoreBuildErrors: true },   // TS 타입 에러 때문에 빌드 멈추지 않게
-};
-
-export default nextConfig;
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+// NOTE: This file should not be edited
